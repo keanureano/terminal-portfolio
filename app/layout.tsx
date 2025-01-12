a5395c5679a88f02590676ed5c02d9d9ffdc5371
Keanu Reaño <reanokeanu@gmail.com>
2025-01-12 17:08:50 +0800

    feat(layout.tsx): add TerminalContextProvider for terminal integration
    feat(lib/gpt.ts): implement GPT integration for handling user queries
    
    refactor(page.tsx): replace static content with interactive terminal
    
    chore(package.json): update dependencies for react-terminal and openai
    
    chore: remove unused SVG files from public directory to clean up assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TerminalContextProvider } from "react-terminal";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
---
+++ app/lib/gpt.ts
@@ -0,0 +1,44 @@
+"use server";
+import OpenAI from "openai";
+
+const API_KEY = process.env.OPENAI_API_KEY;
+const openai = new OpenAI({ apiKey: API_KEY });
+
+const ASSISTANT_ID = "asst_UtR7kgT7I5BH7kwvJfSGbflg";
+const assistant = await openai.beta.assistants.retrieve(ASSISTANT_ID);
+const thread = await openai.beta.threads.create();
+
+export async function gpt(prompt: string) {
+  try {
+    await openai.beta.threads.messages.create(thread.id, {
+      role: "user",
+      content: prompt,
+    });
+
+    let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+      assistant_id: assistant.id,
+    });
+
+    if (run.status === "completed") {
+      const messages = await openai.beta.threads.messages.list(run.thread_id);
+      const firstMessage = messages.data[0];
+      const firstContent = firstMessage.content[0];
+
+      if (firstContent && "text" in firstContent && firstContent.text) {
+        const result = firstContent.text.value;
+
+        // Remove patterns like [brackets]
+        const filtered_result = result
+          .replace(/【\d+:\d+†source】/g, "")
+          .trim();
+
+        return filtered_result;
+      }
+    } else {
+      console.log(run.status);
+    }
+  } catch (error) {
+    console.error("Error communicating with custom assistant:", error);
+    return "An error occurred while processing your request.";
+  }
+}
